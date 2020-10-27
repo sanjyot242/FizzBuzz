ab87db988c3c8f604fe20d89e4b1f1ab0c5b51df
Sanjyot <41758207+sanjyot242@users.noreply.github.com>
2020-10-27 15:59:51 +0530

    Faliure for Test Case 5 , shouldReturnBuzz_For_5()

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -15,21 +15,25 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldReturnOne()
+    public void shouldReturnOne_For_1()
     {
         assertThat(App.getAnswerFor(1),is("1"));
     }
     @Test
-    public void shouldReturnTwo(){
+    public void shouldReturnTwo_For_2(){
         assertThat(App.getAnswerFor(2),is("2"));
     }
     @Test
-    public void shouldReturnFizz(){
+    public void shouldReturnFizz_For_3(){
         assertThat(App.getAnswerFor(3),is("Fizz"));
     }
     @Test
-    public void shouldReturnFour(){
+    public void shouldReturnFour_For_4(){
         assertThat(App.getAnswerFor(4),is("4"));
+    }
+    @Test
+    public void shouldReturnBuzz_For_5(){
+        assertThat(App.getAnswerFor(5),is("Buzz"));
     }
 
 
