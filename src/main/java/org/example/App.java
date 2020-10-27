ba745d059f8eb543a1f142dc4543a8bedb35e5b0
Sanjyot <41758207+sanjyot242@users.noreply.github.com>
2020-10-27 16:21:18 +0530

    Refactoring

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,14 +13,22 @@ public class App
 
     public static String getAnswerFor(int number) {
         String answer = String.valueOf(number);
-        if(number%15==0){
+        if(isMultipleOfThree(number) && isMultipleOfFive(number)){
             answer = "FizzBuzz";
-        }else if(number%3==0){
+        }else if(isMultipleOfThree(number)){
             answer = "Fizz";
         }
-        else if(number%5==0){
+        else if(isMultipleOfFive(number)){
            answer = "Buzz";
         }
         return answer;
+    }
+
+    private static boolean isMultipleOfFive(int number) {
+        return number%5==0;
+    }
+
+    private static boolean isMultipleOfThree(int number) {
+        return number%3 == 0;
     }
 }
